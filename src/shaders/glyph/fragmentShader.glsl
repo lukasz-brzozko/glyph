57fff2f0905bc45f09f479dffe092b6199bcac3e
Łukasz Brzózko <lukasz9099@gmail.com>
2025-08-27 19:07:43 +0200

    feat: implement simplex noise in vertex shader for elevation adjustments

--- src/shaders/glyph/fragmentShader.glsl
+++ src/shaders/glyph/fragmentShader.glsl
@@ -1,22 +1,14 @@
 uniform sampler2D uTexture;
-uniform vec2 uResolution;
 
 varying vec2 vUv;
 
 void main() {
-  float cells = 30.0;
-  float gridGap = 0.95;
-  float gridX = mod(vUv.x * cells, 1.0);
-  gridX = 1.0 - step(gridGap, gridX);
-
-  float gridY = mod(vUv.y * cells, 1.0);
-  gridY = 1.0 - step(gridGap, gridY);
-
-  // vec4 textureColor = texture2D(uTexture, vUv);
-  // vec4 strength = gridX * gridY * textureColor;
-  // gl_FragColor = vec4(strength.xyz, gridX * gridY);
+  vec2 uv = gl_PointCoord;
+  float distanceToCenter = length(uv - 0.5);
+  float alpha = 0.05 / distanceToCenter - 0.1;
 
   vec4 textureColor = texture2D(uTexture, vUv);
 
-  gl_FragColor = textureColor;
+  gl_FragColor = vec4(textureColor.rgb, alpha);
+  // gl_FragColor = vec4(color);
 }
