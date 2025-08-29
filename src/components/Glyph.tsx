d73050f82d560651f23da9a788056af809799db5
Łukasz Brzózko <lukasz9099@gmail.com>
2025-08-29 20:00:08 +0200

    feat: implement dynamic user texture loading in Glyph component

--- src/components/Glyph.tsx
+++ src/components/Glyph.tsx
@@ -1,24 +1,37 @@
 import { useFrame, useLoader, useThree } from "@react-three/fiber";
 import { useControls } from "leva";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import {
   AdditiveBlending,
   DoubleSide,
   type Mesh,
   type ShaderMaterial,
+  Texture,
   TextureLoader,
   Uniform,
   Vector2,
 } from "three";
 
+import useStore from "@/redux/store";
 import glyphFragmentShader from "@/shaders/glyph/fragmentShader.glsl";
 import glyphVertexShader from "@/shaders/glyph/vertexShader.glsl";
 
 function Glyph() {
+  const { image } = useStore();
   const meshRef = useRef<Mesh | null>(null);
   const materialRef = useRef<null | ShaderMaterial>(null);
   const { gl, size } = useThree();
-  const glyphTexture = useLoader(TextureLoader, "birdy.webp");
+  // const glyphTexture = useLoader(TextureLoader, "birdy.webp");
+  const glyphTexture = useLoader(
+    TextureLoader,
+    "juz-nie-chce-urlopu-removebg-preview.png",
+  );
+  const userTexture = useLoader(
+    TextureLoader,
+    "juz-nie-chce-urlopu-removebg-preview.png",
+  );
+  userTexture.repeat.set(1, 1);
+  // const [userTexture, setUserTexture] = useState<null | Texture>(null);
 
   const uniformsRef = useRef({
     uAmplitude: new Uniform(0.35),
@@ -35,29 +48,29 @@ function Glyph() {
     uAmplitude: {
       max: 5,
       min: 0,
+      step: 0.01,
+      value: uniformsRef.current.uAmplitude.value,
       onChange: (newValue) => {
         uniformsRef.current.uAmplitude.value = newValue;
       },
-      step: 0.01,
-      value: uniformsRef.current.uAmplitude.value,
     },
     uFrequency: {
       max: 3,
       min: 0,
+      step: 0.01,
+      value: uniformsRef.current.uFrequency.value,
       onChange: (newValue) => {
         uniformsRef.current.uFrequency.value = newValue;
       },
-      step: 0.01,
-      value: uniformsRef.current.uFrequency.value,
     },
     uSpeed: {
       max: 6,
       min: 0,
+      step: 0.01,
+      value: uniformsRef.current.uSpeed.value,
       onChange: (newValue) => {
         uniformsRef.current.uSpeed.value = newValue;
       },
-      step: 0.01,
-      value: uniformsRef.current.uSpeed.value,
     },
   });
   gl.setPixelRatio(Math.min(window.devicePixelRatio, 2));
@@ -70,6 +83,37 @@ function Glyph() {
   useEffect(() => {
     uniformsRef.current.uResolution.value.set(size.width, size.height);
   }, [size]);
+
+  useEffect(() => {
+    let imageEl: HTMLImageElement | null = null;
+    let imgUrl: null | string = null;
+
+    const onImageLoad = (event: Event) => {
+      console.log(event.target);
+
+      userTexture.image = event.target;
+      userTexture.needsUpdate = true;
+    };
+
+    if (image) {
+      imgUrl = URL.createObjectURL(image);
+      imageEl = new Image();
+      imageEl.addEventListener("load", onImageLoad);
+      imageEl.src = imgUrl;
+
+      uniformsRef.current.uTexture.value = userTexture;
+    } else {
+      uniformsRef.current.uTexture.value = glyphTexture;
+    }
+
+    return () => {
+      userTexture?.dispose();
+      URL.revokeObjectURL(imgUrl || "");
+      imageEl?.removeEventListener("load", onImageLoad);
+      imgUrl = null;
+      imageEl = null;
+    };
+  }, [image, glyphTexture, userTexture]);
 
   return (
     <points ref={meshRef}>
